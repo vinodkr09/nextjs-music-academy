9b4bfaec1de9c63f40d3b150c47d9e8a96647436
vinodkr09 <vinod.krmahi@gmail.com>
2024-08-08 15:58:28 +0530

    done with Navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0/96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">chai aur code </h1>
+    </main>
+  );
+}
