bc001f4dffc5ac932d8e96389d9e8197ed006830
vinodkr09 <vinod.krmahi@gmail.com>
2024-08-10 22:19:28 +0530

    done with why choose us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestmonialsCard from "@/components/TestmonialsCard";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0/96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
+      <MusicSchoolTestmonialsCard/>
     </main>
   );
 }
